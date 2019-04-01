f0d468e2536cba360b17d5e78234e9e4ad1e3b89
Забоев Эдуард <ed.zaboev@digital-sector.ru>
2019-04-01 16:47:24 +0300

    init

---
+++ run_from_this_dir.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#git clone https://github.com/sleppysmile/zadaniya sleepysmile
+#git clone https://github.com/Tory-Tory/php tory-tory
+#git clone https://github.com/nickaname/lessons nickaname
+
+cd sleepysmile
+git pull origin master
+cd -
+cd tory-tory
+git pull origin master
+cd -
+cd nickaname
+git checkout les-1 
+git pull origin les-1
+
+
